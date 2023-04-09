8dbe77d4071440c77598f40faca374e6f616bb3b
yusrilarzaqi <arzaqiyusril@gmail.com>
2023-04-09 15:17:01 +0700

    7.3 Starting Relative Paths with `super`

--- managing_growing_projects_with_packages_crates_and_modules/src/lib.rs
+++ managing_growing_projects_with_packages_crates_and_modules/src/lib.rs
@@ -13,3 +13,14 @@ pub fn eat_at_restaurant() {
     // Relative path
     front_of_house::hosting::add_to_waitlist();
 }
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
