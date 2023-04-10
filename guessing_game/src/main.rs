00ea5d3d387802759488ad13eb8cf0b524bee978
yusrilarzaqi <arzaqiyusril@gmail.com>
2023-04-10 15:27:13 +0700

    7.4 Using Nested Paths to Clean Up Large `use` Lists

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,5 @@
 use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+use std::{cmp::Ordering, io};
 
 fn main() {
     let num = io::stdout; 
--- managing_growing_projects_with_packages_crates_and_modules/src/main.rs
+++ managing_growing_projects_with_packages_crates_and_modules/src/main.rs
@@ -1,28 +1,5 @@
-use std::collections::HashMap;
-
-#[derive(Hash, Eq, PartialEq, Debug)]
-struct Viking {
-    name: String,
-    country: String,
-}
-
-impl Viking {
-    fn new(name: &str, country: &str) -> Viking {
-        Viking {
-            name: name.to_string(),
-            country: country.to_string(),
-        }
-    }
-}
+use std::io::{self, Write};
 
 fn main() {
-    let vikings = HashMap::from([
-        (Viking::new("Ibex", "Iceland"), 34),
-        (Viking::new("Wombat", "Findland"), 25),
-        (Viking::new("Olaf", "Norway"), 40)
-    ]);
-
-    for (viking, health) in &vikings {
-        println!("{viking:?} has {health} hp");
-    }
+    let num = io::stdin();
 }
