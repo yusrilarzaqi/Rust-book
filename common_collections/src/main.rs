6a3e07ea71e65c5b6600a33904db70a9cbd4c5dc
yusrilarzaqi <arzaqiyusril@gmail.com>
2023-04-10 22:55:16 +0700

    Updating a Vector

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
-    // let v: Vec<i32> = Vec::new();
-    let v = vec![1, 2, 3];
+    let mut v = Vec::new();
 
-    println!("{v:#?}");
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    println!("v : {v:#?}");
 }
