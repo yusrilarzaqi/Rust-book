b4737c0f5ab69db8aced88eced22ef788c2ec448
yusrilarzaqi <arzaqiyusril@gmail.com>
2023-04-12 00:38:03 +0700

    8.1 Iterating over the Values in a Vector

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
-    let mut v = vec![1, 2, 3, 4, 5];
+    let mut v = vec![100, 32, 57];
 
-    let first = &v[0];
-
-    v.push(6);
-
-    println!("The first element is: {first}");
+    for i in &v {
+        *i += 50;
+    }
 }
