f7510285d9902a77cf887acb83ef3423be438bf4
yusrilarzaqi <arzaqiyusril@gmail.com>
2023-04-13 04:43:32 +0700

    8.2 Concatenating with the `+` Operator or the `format!` Macro

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -1,14 +1,43 @@
-fn main() {}
+fn main() {
+    concatenation_with_format_macro();
+}
 
+#[allow(dead_code)]
 fn push_method() {
     let mut s = String::from("lo");
     s.push('l');
     println!("s: {s}");
 }
 
+#[allow(dead_code)]
 fn push_str_method() {
     let mut s1 = String::from("foo");
     let s2 = "bar";
     s1.push_str(s2);
     println!("s2: {s2}");
+}
+
+#[allow(dead_code)]
+fn concatenation_with_operator() {
+    // let s1 = String::from("Hello, ");
+    // let s2 = String::from("world!");
+    // let s3 = s1 + &s2; // note s1 has been moved here and ccan no longer be used
+    //
+    // println!("s3: {s3}");
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+    println!("s: {s}");
+}
+
+fn concatenation_with_format_macro() {
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{s1}-{s2}-{s3}");
+    println!("s: {s}");
 }
