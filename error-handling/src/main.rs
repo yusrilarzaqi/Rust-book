855395dac4ce0f379761ed8d9cb70a1ec8be11b9
yusrilarzaqi <arzaqiyusril@gmail.com>
2023-05-02 20:30:36 +0700

    9.2 A Shortcut for Propagating Errors: the `?` Operator

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,23 +1,11 @@
-use std::{io::{self, Read} , fs::File};
+use std::{io, fs};
 
-fn main() {
-    let result = read_username_from_file();
-
-    println!("{:?}", result.unwrap());
+fn read_username_from_flle() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
 }
 
-fn read_username_from_file() -> Result<String, io::Error> {
-    let username_file_result = File::open("hello.txt");
+fn main() {
+    let result = read_username_from_flle();
 
-    let mut username_file = match username_file_result {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
-
-    let mut username = String::new();
-
-    match username_file.read_to_string(&mut username) {
-        Ok(_) => Ok(username),
-        Err(e) => Err(e),
-    }
+    println!("{}", result.unwrap());
 }
